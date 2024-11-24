bbe69574a65e4c92a5492757358760e7a3bcdf3a
tori.ham <tori.ham@kakaocorp.com>
2024-11-24 23:42:13 +0900

    2914

--- 2914.ts
+++ 2914.ts
@@ -1,18 +1,11 @@
 function minChanges(s: string): number {
     let answer = 0
-    let prevCount = 1
-    const strArr = s.split('')
 
-    for (let i = 1; i < strArr.length; i++) {
-        if (strArr[i - 1] !== strArr[i] && prevCount % 2 == 1) {
-            strArr.splice(i, 1, s[i-1])
-            prevCount = 1
-            i++
+    for (let i = 1; i < s.length; i += 2) {
+        if (s[i - 1] !== s[i]) {
             answer++
         }
-        else prevCount++
     }
 
     return answer
-
 };
